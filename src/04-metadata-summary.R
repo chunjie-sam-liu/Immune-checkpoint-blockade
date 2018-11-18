f913defaccf3bc06364a624d6338b685770515a4
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2018-11-18 22:19:19 +0800

    meta data summary

---
+++ src/04-metadata-summary.R
@@ -0,0 +1,9 @@
+
+# library -----------------------------------------------------------------
+
+library(magrittr)
+
+
+# analysis ----------------------------------------------------------------
+
+public_metadata <- readxl::read_xlsx(path = 'data/02-metadata-verified.xlsx', sheet = 1)
