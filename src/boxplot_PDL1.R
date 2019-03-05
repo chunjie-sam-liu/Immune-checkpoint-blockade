d628e80807e4f96c26929a36e44ea09a573c392f
liulanlan812 <liull_hust@hust.edu.cn>
2019-03-05 19:28:24 +0800

    PDL1

--- src/boxplot_PDL1.R
+++ src/boxplot_PDL1.R
@@ -7,7 +7,7 @@ colnames(melanoma_PD1)[1]="ensembl_ID"
 dplyr::filter(melanoma_PD1,ensembl_ID=="ENSG00000120217")->PDL1
 
 
-readxl::read_excel("/data/liull/immune-checkpoint-blockade/all_metadata_available.xlsx",col_names = TRUE,sheet="SRA") %>%
+readxl::read_excel("/data/liucj/data/immune-checkpoint-blockade/all_metadata_available.xlsx",col_names = TRUE,sheet="SRA") %>%
   dplyr::filter(Library_strategy=="RNA-Seq") %>%
   dplyr::filter(Cancer=="melanoma") %>%
   dplyr::filter(Anti_target=="anti-PD1") %>%
@@ -17,23 +17,19 @@ readxl::read_excel("/data/liull/immune-checkpoint-blockade/all_metadata_availabl
 dplyr::filter(metadata,Response %in% c("CR","PR","PRCR","R")) -> response
 dplyr::filter(metadata,Response %in% c("SD","PD","NR")) -> non_response
 
-group=c(rep("CR/PR",nrow(response)),rep("SD/PD",nrow(non_response)))
-dplyr::select(PDL1,response$Run,non_response$Run) %>%
-  rbind(group)%>%
+group_1=c(rep("CR/PR",nrow(response)),rep("SD/PD",nrow(non_response)))
+dplyr::select(PDL1,response$Run,non_response$Run)%>%
+  rbind(group_1)%>%
   t()%>%
-  as.data.frame()->ordered_PDL1
-colnames(ordered_PDL1)=c("PDL1","group")
-ordered_PDL1$PDL1=as.numeric(ordered_PDL1$PDL1)
+  as.data.frame()->ordered_PDL1_1
+colnames(ordered_PDL1_1)=c("PDL1","group")
+ordered_PDL1_1$PDL1=as.numeric(as.character(ordered_PDL1_1$PDL1))
+ordered_PDL1_1$PDL1[which(ordered_PDL1_1$PDL1[]<0)]=0.001
+ordered_PDL1_1$PDL1=log10(ordered_PDL1_1$PDL1)
 
-ggplot(ordered_PDL1,aes(x=group,y=PDL1,fill=group))+geom_boxplot()+labs(title="melanoma_PD1")->boxplot_melanoma_PDL1
-ggsave(
-  filename = 'boxplot_melanoma_PD1_PDL1.pdf',
-  plot = boxplot_melanoma_PDL1,
-  device = 'pdf',
-  path = '/data/liull/immune-checkpoint-blockade/different_expression',
-  width = 6,
-  height = 6.8
-)
+
+#ggplot(ordered_PDL1_1)+geom_boxplot(aes(x=group_1,y=PDL1,fill=group_1))->boxplot_melanoma_PDL1
+
 #2-----------------------------------------------------------------------------------
 read.table("/data/liull/immune-checkpoint-blockade/different_expression/melanoma/melanoma_CTLA4_removed_batch_expression.txt",header = T,as.is = TRUE) ->melanoma_CTLA4
 cbind(rownames(melanoma_CTLA4),melanoma_CTLA4)->melanoma_CTLA4
@@ -42,8 +38,8 @@ colnames(melanoma_CTLA4)[1]="ensembl_ID"
 dplyr::filter(melanoma_CTLA4,ensembl_ID=="ENSG00000120217")->PDL1
 
 
-readxl::read_excel("/data/liull/immune-checkpoint-blockade/all_metadata_available.xlsx",col_names = TRUE,sheet="SRA") -> SRA
-readxl::read_excel("/data/liull/immune-checkpoint-blockade/all_metadata_available.xlsx",col_names = TRUE,sheet="dbGAP") -> dbGAP  
+readxl::read_excel("/data/liucj/data/immune-checkpoint-blockade/all_metadata_available.xlsx",col_names = TRUE,sheet="SRA") -> SRA
+readxl::read_excel("/data/liucj/data/immune-checkpoint-blockade/all_metadata_available.xlsx",col_names = TRUE,sheet="dbGAP") -> dbGAP  
   rbind(SRA,dbGAP) %>%
   dplyr::filter(Library_strategy=="RNA-Seq") %>%
   dplyr::filter(Cancer=="melanoma") %>%
@@ -54,27 +50,21 @@ readxl::read_excel("/data/liull/immune-checkpoint-blockade/all_metadata_availabl
 dplyr::filter(metadata,Response %in% c("CR","PR","PRCR","R")) -> response
 dplyr::filter(metadata,Response %in% c("SD","PD","NR")) -> non_response
 
-group=c(rep("CR/PR",nrow(response)),rep("SD/PD",nrow(non_response)))
+group_2=c(rep("CR/PR",nrow(response)),rep("SD/PD",nrow(non_response)))
 dplyr::select(PDL1,response$Run,non_response$Run) %>%
-  rbind(group)%>%
+  rbind(group_2)%>%
   t()%>%
-  as.data.frame()->ordered_PDL1
-colnames(ordered_PDL1)=c("PDL1","group")
-ordered_PDL1$PDL1=as.numeric(ordered_PDL1$PDL1)
+  as.data.frame()->ordered_PDL1_2
+colnames(ordered_PDL1_2)=c("PDL1","group")
+ordered_PDL1_2$PDL1=as.numeric(as.character(ordered_PDL1_2$PDL1))
+ordered_PDL1_2$PDL1[which(ordered_PDL1_2$PDL1[]<0)]=0.001
+ordered_PDL1_2$PDL1=log10(ordered_PDL1_2$PDL1)
 
-ggplot(ordered_PDL1,aes(x=group,y=PDL1,fill=group))+geom_boxplot()+labs(title="melanoma_CTLA4")->boxplot_melanoma_CTLA4
-ggsave(
-  filename = 'boxplot_melanoma_CTLA4_PDL1.pdf',
-  plot = boxplot_melanoma_CTLA4,
-  device = 'pdf',
-  path = '/data/liull/immune-checkpoint-blockade/different_expression',
-  width = 6,
-  height = 6.8
-)
+#ggplot(ordered_PDL1_2)+geom_boxplot(aes(x=group,y=PDL1,fill=group))->boxplot_melanoma_CTLA4
 
 #3----------------------------------------------------------------
 
-readxl::read_excel("/data/liull/immune-checkpoint-blockade/all_metadata_available.xlsx",col_names = TRUE,sheet="SRA") %>%
+readxl::read_excel("/data/liucj/data/immune-checkpoint-blockade/all_metadata_available.xlsx",col_names = TRUE,sheet="SRA") %>%
   dplyr::filter(Library_strategy=="RNA-Seq") %>%
   dplyr::filter(Cancer=="gastric cancer") %>%
   dplyr::filter(Anti_target=="anti-PD1") %>%
@@ -84,22 +74,45 @@ rbind(dplyr::filter(metadata,Response=="CR"),dplyr::filter(metadata,Response=="P
 rbind(dplyr::filter(metadata,Response=="SD"),dplyr::filter(metadata,Response=="PD")) ->non_response
 
 
-read.table("/data/liull/immune-checkpoint-blockade/expression/all_FPKM_expression_2.txt",sep="\t",header = T,as.is = TRUE) %>%
-  dplyr::select(gene_id,response$Run,non_response$Run)%>%
-  dplyr::filter(gene_id=="ENSG00000120217")->PDL1
+read.table("/data/liull/immune-checkpoint-blockade/different_expression/gastric_cancer/gastric_cancer_PD1_DEG.txt",sep="\t",header = T,as.is = TRUE) %>%
+  dplyr::select(ensembl_ID,response$Run,non_response$Run)%>%
+  dplyr::filter(ensembl_ID=="ENSG00000120217")->PDL1
 
-group=c("group",rep("CR/PR",nrow(response)),rep("SD/PD",nrow(non_response)))
-rbind(PDL1,group)->ordered_PDL1
-rownames(ordered_PDL1)=c("PDL1","group")
-ordered_PDL1=ordered_PDL1[,-1]
-ordered_PDL1=as.data.frame(t(ordered_PDL1))
-ordered_PDL1$PDL1=as.numeric(ordered_PDL1$PDL1)
-ggplot(ordered_PDL1,aes(x=group,y=PDL1,fill=group))+geom_boxplot()+labs(title="gastric_cancer_PD1")->boxplot_gastric_cancer_PDL1
+group_3=c(rep("CR/PR",nrow(response)),rep("SD/PD",nrow(non_response)))
+PDL1[,-1]%>%
+  rbind(group_3)%>%
+  t()%>%
+  as.data.frame()->ordered_PDL1_3
+
+colnames(ordered_PDL1_3)=c("PDL1","group")
+
+ordered_PDL1_3$PDL1=log10(as.numeric(as.character(ordered_PDL1_3$PDL1)))
+ordered_PDL1_3$PDL1=as.numeric(as.character(ordered_PDL1_3$PDL1))
+#ggplot(ordered_PDL1)+geom_boxplot(aes(x=group,y=PDL1,fill=group))->boxplot_gastric_cancer_PDL1
+
+
+my_comparisons <- list(c("CR/PR", "SD/PD"))
+ggboxplot(ordered_PDL1_1, x="group", y="PDL1", color = "group")+
+  stat_compare_means()->boxplot_melanoma_PD1_PDL1
+ggboxplot(ordered_PDL1_2, x="group", y="PDL1", color = "group")+
+  stat_compare_means(comparisons=my_comparisons,method = "t.test")->boxplot_melanoma_CTLA4_PDL1
+ggboxplot(ordered_PDL1_3, x="group", y="PDL1", color = "group")+
+  stat_compare_means()
+
+cbind(rep("melanoma_PD1",nrow(ordered_PDL1_1)),ordered_PDL1_1)->a
+cbind(rep("melanoma_CTLA4",nrow(ordered_PDL1_2)),ordered_PDL1_2)->b
+cbind(rep("gastric_cancer_PD1",nrow(ordered_PDL1_3)),ordered_PDL1_3)->c
+colnames(a)[1]="Cancer"
+colnames(b)[1]="Cancer"
+colnames(c)[1]="Cancer"
+rbind(a,b,c)->all
+p <- ggboxplot(all, x="Cancer", y="PDL1", color = "group", palette = "jco", add = "jitter") + 
+  stat_compare_means(aes(group=group))
 ggsave(
-  filename = 'boxplot_gastric_cancer_PD1_PDL1.pdf',
-  plot = boxplot_gastric_cancer_PDL1,
+  filename = 'boxplot_PDL1.pdf',
+  plot = p,
   device = 'pdf',
   path = '/data/liull/immune-checkpoint-blockade/different_expression',
   width = 6,
   height = 6.8
-)
+  )
