620f1613ddb37b035fdb792fa3094af313fae2ba
liulanlan812 <liull_hust@hust.edu.cn>
2019-03-07 20:28:57 +0800

    diff exp

--- src/07-melanoma-diff-exp-PD1.R
+++ src/07-melanoma-diff-exp-PD1.R
@@ -2,98 +2,43 @@ library(magrittr)
 library(readr)
 library(readxl)
 library(dplyr)
-library(sva)
-#筛选出黑色素瘤的RNA-seq的anti-PD1的数据的应答信息-------------------------------------------------------------
+library(clusterProfiler)
+library(ReactomePA)
+
+#load melanoma_PD1_removed_batch profile
+read.table("/data/liull/immune-checkpoint-blockade/different_expression/melanoma/melanoma_PD1_removed_batch_expression.txt",header = T,as.is = TRUE)->combat_edata
+
+#filter melanoma RNA-seq anti-PD1's response information
 readxl::read_excel("/data/liull/immune-checkpoint-blockade/all_metadata_available.xlsx",col_names = TRUE,sheet="SRA") %>%
   dplyr::filter(Library_strategy=="RNA-Seq") %>%
   dplyr::filter(Cancer=="melanoma") %>%
   dplyr::filter(Anti_target=="anti-PD1") %>%
-  dplyr::select(SRA_Study,Run,Response) ->metadata
+  #dplyr::filter(Biopsy_Time=="pre-treatment")%>%
+  dplyr::select(SRA_Study,Run,Response) ->metadata#87
 
-#select response and non_response's sample id and project id
-dplyr::filter(metadata,Response %in% c("CR","PR","PRCR","R")) -> response
-dplyr::filter(metadata,Response %in% c("SD","PD","NR")) -> non_response
+#select and order profile--response~nonresponse
+dplyr::filter(metadata,Response %in% c("CR","PR","PRCR","R")) -> response#26
+dplyr::filter(metadata,Response %in% c("SD","PD","NR")) -> non_response#59
+dplyr::select(combat_edata,response$Run,non_response$Run)->ordered_melanoma_PD1
 
-#expression prepare for batch effect----------------------------------------------------------------------------
-read.table("/data/liull/immune-checkpoint-blockade/expression/all_FPKM_expression_2.txt",sep="\t",header = T,as.is = TRUE) ->data1
-Project=unique(metadata$SRA_Study)
-dplyr::filter(metadata,SRA_Study==Project[1]) %>%
-  dplyr::select(Run)  %>%
-  as.matrix() %>%
-  as.character()->Project1_id
+avg.R=apply(ordered_melanoma_PD1,1,function(x) median(x[1:nrow(response)]))
+avg.NR=apply(ordered_melanoma_PD1,1,function(x) median(x[(nrow(response)+1):length(ordered_melanoma_PD1)]))
+diff.avg=apply(ordered_melanoma_PD1,1,function(x) (median(x[1:nrow(response)])-median(x[(nrow(response)+1):length(ordered_melanoma_PD1)])))
+p_value=apply(ordered_melanoma_PD1,1,function(x) t.test(x[1:nrow(response)],x[(nrow(response)+1):length(ordered_melanoma_PD1)])$p.value)
+t_statistic=apply(ordered_melanoma_PD1,1,function(x) t.test(x[1:nrow(response)],x[(nrow(response)+1):length(ordered_melanoma_PD1)])$statistic)
 
-dplyr::filter(metadata,SRA_Study==Project[2]) %>%
-  dplyr::select(Run)  %>%
-  as.matrix() %>%
-  as.character()->Project2_id
-
-dplyr::filter(metadata,SRA_Study==Project[3]) %>%
-  dplyr::select(Run)  %>%
-  as.matrix() %>%
-  as.character()->Project3_id
-
-expression1=dplyr::select(data1,gene_id,Project1_id,Project2_id,Project3_id)
-#order the expression profile by project to remove batch effect
-
-Sum_NA=apply(expression1,1,function(x) sum(is.na(x)))
-NA_IDs=which(Sum_NA>=(length(expression1)/4))
-expression2=expression1[-NA_IDs,]#delete the gene has more than 1/4 samples' NA
-
-row.names(expression2)=expression2[,1]
-expression2=expression2[,-1]
-dim(expression2)#make rownames to avoid of sum wrong
-
-Sum_zero=apply(expression2,1,function(x) sum(as.numeric(x),na.rm = TRUE))
-IDs_zero=which(Sum_zero==0)
-expression3=expression2[-IDs_zero,]#delete the gene has all 0.000 depression
-
-a=1:nrow(expression3)
-for(j in 1:length(expression3)){
-  which(expression3[,j]<10) ->b
-  intersect(a,b)->a
-}
-expression4=expression3[-a,]
-
-for(i in 1:length(expression4)) {
-  expression4[is.na(expression4[, i]), i] <- mean(expression4[, i], na.rm = T)
-}#replace NA to mean of its sample expression
-
-
-
-
-#remove batch effect---------------------------------------------------------------------------------
-batch1=rep(1,length(Project1_id))
-batch2=rep(2,length(Project2_id))
-batch3=rep(3,length(Project3_id))
-batch=c(batch1,batch2,batch3)
-expression4=as.matrix(expression4)
-combat_edata = ComBat(dat=expression4, batch=batch, mod=NULL, par.prior=TRUE, prior.plots=FALSE)
-write.table(combat_edata,"/data/liull/immune-checkpoint-blockade/different_expression/melanoma/melanoma_PD1_removed_batch_expression.txt",quote = FALSE,row.names = TRUE,col.names = TRUE)
-
-#make difference----------------------------------------------------------------------------------------
-dplyr::select(as.data.frame(combat_edata),non_response$Run) ->non_response_expression
-dplyr::select(as.data.frame(combat_edata),response$Run) ->response_expression
-all_expression=cbind(response_expression,non_response_expression)
-#orderthe expression profile by response and nonresponse to t.test
-
-avg.R=apply(all_expression,1,function(x) median(x[1:nrow(response)]))
-avg.NR=apply(all_expression,1,function(x) median(x[(nrow(response)+1):length(all_expression)]))
-diff.avg=apply(all_expression,1,function(x) (median(x[1:nrow(response)])-median(x[(nrow(response)+1):length(all_expression)])))
-p_value=apply(all_expression,1,function(x) t.test(x[1:nrow(response)],x[(nrow(response)+1):length(all_expression)])$p.value)
-result=as.data.frame(cbind(all_expression,avg.R,avg.NR,diff.avg,p_value))
-
+result=as.data.frame(cbind(ordered_melanoma_PD1,avg.R,avg.NR,diff.avg,p_value,t_statistic))
 result=cbind(rownames(result),result)
 rownames(result)=NULL
 colnames(result)[1]="ensembl_ID"
 write.table(result,"/data/liull/immune-checkpoint-blockade/different_expression/melanoma/PD1/melanoma_PD1_DEG.txt",quote = FALSE,sep="\t",row.names = FALSE,col.names = TRUE)#write all genes' difference
 
 
-#filter significant different gene-------------------------------------
 dplyr::filter(as.data.frame(result),p_value<=0.05) %>%
-  dplyr::filter(diff.avg>=2) -> up#54
+  dplyr::filter(diff.avg>=2) -> up#36
 
 dplyr::filter(as.data.frame(result),p_value<=0.05) %>%
-  dplyr::filter(diff.avg<=-2) -> down#199
+  dplyr::filter(diff.avg<=-2) -> down#78
 
 read.table("/data/liull/reference/EntrezID_Symbl_EnsemblID_NCBI.txt",sep="\t",header = T,as.is = TRUE) ->relationship
 merge(relationship,up,by.x="EnsemblId",by.y="ensembl_ID",all=TRUE)%>%
@@ -123,7 +68,7 @@ write.table(as.data.frame(ekegg_up),"/data/liull/immune-checkpoint-blockade/diff
 write.table(as.data.frame(ekegg_down),"/data/liull/immune-checkpoint-blockade/different_expression/melanoma/PD1/down_enrichKEGG.txt",quote = FALSE,sep="\t",row.names = FALSE,col.names = TRUE)
 
 #Reactome enrichment------------------------------------------------------
-enrichPathway(gene=up2$GeneID,pvalueCutoff=0.05, readable=T)->eReactome_up#17
+enrichPathway(gene=up2$GeneID,organism="human",pvalueCutoff=0.01, readable=T)->eReactome_up#17
 dotplot(eReactome_up, showCategory=17)
 enrichPathway(gene=down2$GeneID,pvalueCutoff=0.05, readable=T)->eReactome_down#11
 dotplot(eReactome_down, showCategory=11)
