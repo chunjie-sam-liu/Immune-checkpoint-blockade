b604599e57a56b48cb83c0bbf1161d319068ceb1
liulanlan812 <liull_hust@hust.edu.cn>
2019-03-09 20:17:18 +0800

    vennplot

--- src/venn.R
+++ src/venn.R
@@ -1,3 +1,5 @@
+library (VennDiagram)
+
 read.table("/data/liull/immune-checkpoint-blockade/different_expression/gastric_cancer/up.txt",sep="\t",header = T,as.is = TRUE)->gastric_PD1_up
 read.table("/data/liull/immune-checkpoint-blockade/different_expression/gastric_cancer/down.txt",sep="\t",header = T,as.is = TRUE)->gastric_PD1_down
 read.table("/data/liull/immune-checkpoint-blockade/different_expression/melanoma/PD1/up.txt",sep="\t",header = T,as.is = TRUE)->melanoma_PD1_up
@@ -8,3 +10,12 @@ read.table("/data/liull/immune-checkpoint-blockade/different_expression/melanoma
 gastric_PD1=c(gastric_PD1_up$EnsemblId,gastric_PD1_down$EnsemblId)
 melanoma_PD1=c(melanoma_PD1_up$EnsemblId,melanoma_PD1_down$EnsemblId)
 melanoma_CTLA4=c(melanoma_CTLA4_up$EnsemblId,melanoma_CTLA4_down$EnsemblId)
+
+
+setwd("/data/liull/immune-checkpoint-blockade")
+venn.diagram(list(gastric_PD1=gastric_PD1_up$EnsemblId, melanoma_PD1=melanoma_PD1_up$EnsemblId,melanoma_CTLA4=melanoma_CTLA4_up$EnsemblId),filename=NULL,,fill = c("cornflowerblue", "green", "yellow"))->venn_up
+grid.draw(venn_up)
+
+venn.diagram(list(gastric_PD1=gastric_PD1_down$EnsemblId, melanoma_PD1=melanoma_PD1_down$EnsemblId,melanoma_CTLA4=melanoma_CTLA4_down$EnsemblId),filename=NULL,,fill = c("cornflowerblue", "green", "yellow"))->venn_down
+grid.draw(venn_down)
+
