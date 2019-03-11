864263b4c5ecd890909b7cd3143cea9c7da4e522
liulanlan812 <liull_hust@hust.edu.cn>
2019-03-11 20:45:02 +0800

    batch effect

--- src/06-batch-effect-melanoma-CTLA4.R
+++ src/06-batch-effect-melanoma-CTLA4.R
@@ -55,3 +55,69 @@ batch=c(batch1,batch2)
 expression3=as.matrix(expression3)
 combat_edata = ComBat(dat=expression3, batch=batch, mod=NULL, par.prior=TRUE, prior.plots=FALSE)
 write.table(combat_edata,"/data/liull/immune-checkpoint-blockade/different_expression/melanoma/melanoma_CTLA4_removed_batch_expression.txt",quote = FALSE,row.names = TRUE,col.names = TRUE)
+
+
+#MDS test---------------------------------------------------------------------------------
+#MDS before
+a1=as.matrix(dist(t(expression3)))
+voles.mds=cmdscale(a1,k=13,eig=T)
+sum(abs(voles.mds$eig[1:2]))/sum(abs(voles.mds$eig))
+sum((voles.mds$eig[1:2])^2)/sum((voles.mds$eig)^2)
+x = voles.mds$points[,1]
+y = voles.mds$points[,2]
+point_color=c(rep("P1",length(Project1_id)),rep("P2",length(Project2_id)))
+p=ggplot(data.frame(x,y,point_color),aes(x,y,label = colnames(a1),colour=point_color))+
+  geom_point(shape=16,size=1)
+ggsave(
+  filename = 'MDS_before.pdf',
+  plot = p,
+  device = 'pdf',
+  path = '/data/liull/immune-checkpoint-blockade/batch_effect/CTLA4',
+  width = 6,
+  height = 6.8
+)
+
+#MDS after removing batch effect with ComBat
+a2=as.matrix(dist(t(combat_edata)))
+voles.mds_2=cmdscale(a2,k=13,eig=T)
+sum(abs(voles.mds_2$eig[1:2]))/sum(abs(voles.mds_2$eig))
+sum((voles.mds_2$eig[1:2])^2)/sum((voles.mds_2$eig)^2)
+x = voles.mds_2$points[,1]
+y = voles.mds_2$points[,2]
+point_color=c(rep("P1",length(Project1_id)),rep("P2",length(Project2_id)))
+p=ggplot(data.frame(x,y,point_color),aes(x,y,label = colnames(a2),colour=point_color))+
+  geom_point(shape=16,size=1)
+ggsave(
+  filename = 'MDS_ComBat.pdf',
+  plot = p,
+  device = 'pdf',
+  path = '/data/liull/immune-checkpoint-blockade/batch_effect/CTLA4',
+  width = 6,
+  height = 6.8
+)
+
+#PCA test----------------------------------------------------------------------------------
+#before
+pca_result <- prcomp(t(expression3),scale=T) 
+point_color=c(rep("P1",length(Project1_id)),rep("P2",length(Project2_id)))
+ggplot(data.frame(pca_result$x[,1:2],point_color),aes(pca_result$x[,1],pca_result$x[,2],label = colnames(expression3),colour=point_color)) + geom_point(shape=16,size=1)->PCA_plot
+ggsave(
+  filename = 'PCA_before.pdf',
+  plot = PCA_plot,
+  device = 'pdf',
+  path = '/data/liull/immune-checkpoint-blockade/batch_effect/CTLA4',
+  width = 6,
+  height = 6.8
+)
+
+#combat
+pca_result <- prcomp(t(combat_edata),scale=T) 
+ggplot(data.frame(pca_result$x[,1:2],point_color),aes(pca_result$x[,1],pca_result$x[,2],label = colnames(combat_edata),colour=point_color)) + geom_point(shape=16,size=1)->PCA_plot
+ggsave(
+  filename = 'PCA_ComBat.pdf',
+  plot = PCA_plot,
+  device = 'pdf',
+  path = '/data/liull/immune-checkpoint-blockade/batch_effect/CTLA4',
+  width = 6,
+  height = 6.8
+)
