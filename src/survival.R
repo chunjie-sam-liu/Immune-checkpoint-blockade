d86109b53eda619c4d7025fff8b2255a924321b7
liulanlan812 <liull_hust@hust.edu.cn>
2019-03-19 20:06:24 +0800

    survival

--- src/survival.R
+++ src/survival.R
@@ -15,13 +15,12 @@ ECM_deg=c("COL1A1","COL1A2","COL3A1","COL5A1","COL5A2","COL6A1","COL6A2","COL6A3
 ECM_org=c("CRTAP","FBLN5","P3H3","COL1A1","COL1A2","COL3A1","COL5A1","COL5A2","COL6A1","COL6A2","COL6A3","COL9A3","COL12A1","COL15A1","COL16A1","VCAN","CAPN12","CTSK","DCN","ELN","FBLN1","TNC","ITGA6","ITGA5","ITGA7","AGRN","KDR","LAMA4","LAMC1","LOXL1","LTBP3","LUM","MFAP1","MFAP2","MFAP4","MMP2","MMP11","MMP14","MMP17","NID1","P4HB","PDGFB","PECAM1","PLEC","P3H2","HTRA1","PTPRS","P3H1","BMP1","SPARC","TGFB3","VWF","PXDN","MFAP5","COL18A1","CAPN2","COL27A1","SERPINH1","ADAM15","P4HA2","PLOD3","ADAMTS4","ADAMTS2","ADAMTS1")
 Collagen_form=c("CRTAP","P3H3","COL1A1","COL1A2","COL3A1","COL5A1","COL5A2","COL6A1","COL6A2","COL6A3","COL9A3","COL12A1","COL15A1","COL16A1","ITGA6","P4HB","PLEC","P3H1","BMP1","PXDN","COL27A1","SERPINH1","P4HA2","PLOD3","ADAMTS2")
 
-up_melanoma_PD1_gastric=c("CD74","GZMA","CCL5")
 
-down_melanoma_PD1_CTLA4_Interaction=c("UBE2E3","AP3M2","GLOD4","MRPS6")
-down_melanoma_PD1_gastric=c("TNC","MMP2","MFAP2","MYO10","SERPINE2","PPFIBP1","PIR","BUD31","DNAJC13","PHF20L1","PIP4P2","GADD45GIP1")
-down_melanoma_CTLA4_gastric_PD1=c("ELN","NID1","GJA1","ATP2B1","GFPT1","NR3C1","CNOT3","PPFIBP2","NDRG1","FSTL1","STXBP5","TRIM74")
+down_melanoma_PD1_CTLA4_Interaction=c("UBE2E3","MRC2")
+down_melanoma_PD1_gastric=c("MMP2","PIR","CYBRD1","COL6A1","COL3A1")
+down_melanoma_CTLA4_gastric_PD1=c("NID1","ATP2B1","FSTL1","STXBP5","NDRG1")
 
-union(Collagen_deg,ECM_deg) %>% union(ECM_org) %>% union(Collagen_form) %>% union(up_melanoma_PD1_gastric) %>% 
+union(Collagen_deg,ECM_deg) %>% union(ECM_org) %>% union(Collagen_form) %>% 
   union(down_melanoma_PD1_CTLA4_Interaction)%>%union(down_melanoma_PD1_gastric)%>%
   union(down_melanoma_CTLA4_gastric_PD1) -> all_genes
 
@@ -33,7 +32,7 @@ readxl::read_excel("/data/liull/immune-checkpoint-blockade/all_metadata_availabl
   dplyr::filter(Biopsy_Time=="pre-treatment")%>%###
   dplyr::select(Run,Survival_time,Survival_status) ->metadata#SRA all anti-PD1,dnGAP all anti-CTLA4
 
-#filter gene and sample  c(Collagen_deg,ECM_deg,ECM_org,Collagen_form) or c("CD74","GZMA","CCL5")...
+#filter gene and sample ...
 expr2 %>%
   dplyr::filter(Symbol %in% all_genes)%>%
   dplyr::select(Symbol,metadata$Run)->expr3
@@ -60,8 +59,6 @@ Combined_data$Survival_status=as.numeric(Combined_data$Survival_status)
 
 #res.cox <- coxph(Surv(Survival_time, Survival_status) ~DCN , data = Combined_data)
 
-
-#covariates <- c("DCN","CD74","VCAN","TNC","ELN","AP3M2","ATP2B1","P4HA2","FBLN1","COL16A1","ADAMTS2","MMP2","PIR","CNOT3","P3H2","ITGA6","COL9A3","MMP11","PDGFB","NDRG1","PTPRS","BUD31","PLOD3","COL1A1","VWF","P3H3","PPFIBP1","COL12A1","LAMA4","SPARC","NR3C1","NID1","MFAP2","P3H1","TGFB3","KDR","LOXL1","PHF20L1","PXDN","COL5A1","ITGA7","LAMC1","SERPINE2","DNAJC13","LUM","FBLN5","MFAP1","COL6A1","COL6A2","CTSK","ADAM15","MYO10","GZMA","SERPINH1","GJA1","ADAMTS1","PIP4P2","MMP14","ADAMTS4","ITGA5","CAPN2","COL6A3","FSTL1","STXBP5","COL1A2","HTRA1","PPFIBP2","MFAP4","GLOD4","LTBP3","BMP1","COL3A1","UBE2E3","CRTAP","TRIM74","PLEC","GADD45GIP1","CAPN12","COL18A1","P4HB","AGRN","COL27A1","MFAP5","GFPT1","MMP17","COL5A2","COL15A1","MRPS6","PECAM1","CCL5" )
 
 #Univariate Cox regression
 covariates <- colnames(Combined_data)[-c(1,2,3)]
