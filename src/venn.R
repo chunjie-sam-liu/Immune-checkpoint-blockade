297c0b65871d72d5f4bffc8e38e1c431f3700088
liulanlan812 <liull_hust@hust.edu.cn>
2019-04-09 20:18:10 +0800

    New result venn

--- src/venn.R
+++ src/venn.R
@@ -1,7 +1,7 @@
 library (VennDiagram)
 
-read.table("/data/liull/immune-checkpoint-blockade/different_expression/gastric_cancer/up.txt",sep="\t",header = T,as.is = TRUE)->gastric_PD1_up
-read.table("/data/liull/immune-checkpoint-blockade/different_expression/gastric_cancer/down.txt",sep="\t",header = T,as.is = TRUE)->gastric_PD1_down
+read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/gastric_cancer/up.txt",sep="\t",header = T,as.is = TRUE)->gastric_PD1_up
+read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/gastric_cancer/down.txt",sep="\t",header = T,as.is = TRUE)->gastric_PD1_down
 read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/PD1_up.txt",header = T,as.is = TRUE)->melanoma_PD1_up
 read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/PD1_down.txt",header = T,as.is = TRUE)->melanoma_PD1_down
 read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_CTLA4/CTLA4_up.txt",header = T,as.is = TRUE)->melanoma_CTLA4_up
@@ -46,4 +46,11 @@ intersect(melanoma_PD1_down$rowname,melanoma_CTLA4_down$rowname)
 intersect(melanoma_PD1_up$rowname,melanoma_CTLA4_up$rowname)
 #CLEC17A  ,DAZ1  ,DAZ4  ,CR2  ,DAZ2
 
+
+intersect(gastric_PD1_up$Ensembl_ID,melanoma_PD1_up$rowname)
+#IDO1, JAKMIP1,TRAT1,LRG1,LCK ,NELL2,PDCD1,HLA-DQA1 ,HLA-DOA ,HCP5,UBD ,LGALS17A,OR2I1P
+#"NONHSAG213716"  "NONHSAG223513"  "NONHSAG223686" "NONHSAG224412" "NONHSAG268497" "NONHSAG268538" "NONHSAG270697""NONHSAG280970"
+intersect(gastric_PD1_down$Ensembl_ID,melanoma_PD1_down$rowname)
+#VCAN,VEGFD,GFRA3,MMP2,NTF3,ROS1,CORO2B,OLFM1,RGS22,NSG1,TM6SF2,MUC13,ANO2,HPSE2,HIF3A,ADAM33,MAGED4B,MS4A8,ESYT3,CCDC8,SCUBE3,RCOR2,RSPO4,ZNF662,MAGED4
+#"NONHSAG229342"   "NONHSAG229343"   "NONHSAG229344"  "NONHSAG260990"   "NONHSAG265471"   "NONHSAG284812" 
 
