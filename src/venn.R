fa74f7e440030920dec207b66f9f381e3f860303
liulanlan812 <liull_hust@hust.edu.cn>
2019-04-16 20:16:13 +0800

    venn

--- src/venn.R
+++ src/venn.R
@@ -1,11 +1,11 @@
 library (VennDiagram)
 
-read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/gastric_cancer/up.txt",sep="\t",header = T,as.is = TRUE)->gastric_PD1_up
-read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/gastric_cancer/down.txt",sep="\t",header = T,as.is = TRUE)->gastric_PD1_down
-read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/PD1_up.txt",header = T,as.is = TRUE)->melanoma_PD1_up
-read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/PD1_down.txt",header = T,as.is = TRUE)->melanoma_PD1_down
-read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_CTLA4/CTLA4_up.txt",header = T,as.is = TRUE)->melanoma_CTLA4_up
-read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_CTLA4/CTLA4_down.txt",header = T,as.is = TRUE)->melanoma_CTLA4_down
+read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/gastric_cancer/up_ENSG.txt",header = T,as.is = TRUE)->gastric_PD1_up
+read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/gastric_cancer/down_ENSG.txt",header = T,as.is = TRUE)->gastric_PD1_down
+read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/PD1_up_ENSG.txt",header = T,as.is = TRUE)->melanoma_PD1_up
+read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/PD1_down_ENSG.txt",header = T,as.is = TRUE)->melanoma_PD1_down
+read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_CTLA4/CTLA4_up_ENSG.txt",header = T,as.is = TRUE)->melanoma_CTLA4_up
+read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_CTLA4/CTLA4_down_ENSG.txt",header = T,as.is = TRUE)->melanoma_CTLA4_down
 
 
 venn.diagram(list(gastric_PD1=gastric_PD1_up$EnsemblId, melanoma_PD1=melanoma_PD1_up$EnsemblId,melanoma_CTLA4=melanoma_CTLA4_up$EnsemblId),filename=NULL,fill = c("cornflowerblue", "green", "yellow"))->venn_up
@@ -50,7 +50,7 @@ intersect(melanoma_PD1_up$rowname,melanoma_CTLA4_up$rowname)
 intersect(gastric_PD1_up$Ensembl_ID,melanoma_PD1_up$rowname)
 #IDO1, JAKMIP1,TRAT1,LRG1,LCK ,NELL2,PDCD1,HLA-DQA1 ,HLA-DOA ,HCP5,UBD ,LGALS17A,OR2I1P
 #"NONHSAG213716"  "NONHSAG223513"  "NONHSAG223686" "NONHSAG224412" "NONHSAG268497" "NONHSAG268538" "NONHSAG270697""NONHSAG280970"
-intersect(gastric_PD1_down$Ensembl_ID,melanoma_PD1_down$rowname)
+intersect(gastric_PD1_down$Symbol,melanoma_PD1_down$Symbol)
 #VCAN,VEGFD,GFRA3,MMP2,NTF3,ROS1,CORO2B,OLFM1,RGS22,NSG1,TM6SF2,MUC13,ANO2,HPSE2,HIF3A,ADAM33,MAGED4B,MS4A8,ESYT3,CCDC8,SCUBE3,RCOR2,RSPO4,ZNF662,MAGED4
 #"NONHSAG229342"   "NONHSAG229343"   "NONHSAG229344"  "NONHSAG260990"   "NONHSAG265471"   "NONHSAG284812" 
 
