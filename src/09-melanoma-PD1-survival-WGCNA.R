4a813834b82b87e7fe8de88ba3e9375642a156e9
liulanlan812 <liull_hust@hust.edu.cn>
2019-04-19 19:06:07 +0800

    survival modules

--- src/09-melanoma-PD1-survival-WGCNA.R
+++ src/09-melanoma-PD1-survival-WGCNA.R
@@ -53,88 +53,167 @@ dplyr::select(as.data.frame(melanoma_PD1_expr1,stringsAsFactors=FALSE),non_respo
 write.table(expr_R,"/data/liull/test2/expr_R.txt",row.names = TRUE,col.names = TRUE,quote=FALSE,sep="\t")
 write.table(expr_NR,"/data/liull/test2/expr_NR.txt",row.names = TRUE,col.names = TRUE,quote=FALSE,sep="\t")
 
-#test genes and samples---------------------------------------------------------------------------------
-gsg = goodSamplesGenes(expr, verbose = 3)
-# Flagging genes and samples with too many missing values...
-# ..step 1
- gsg$allOK
-# [1] TRUE   
-#no need to delete genes
 
-sampleTree = hclust(dist(expr), method = "average");
-jpeg('sample_cluster.jpeg',width = 1000, height = 500, units = "px", pointsize = 6,quality = 100,bg = "#e5ecff",res=100)
-plot(sampleTree,sub="", xlab="")
+#WGCNA3.r expr_R.txt 0.85 30 0.25
+#WGCNA3.r expr_NR.txt 0.85 30 0.25
+#with the sets WGCNA get,do Univariate Cox regression----------------------------------------------------------------------------------
+library(magrittr)
+read.table("/data/liull/test_WGCNA_R/module.color.txt",header = F,as.is = TRUE,skip = 1) ->color_module
+
+cbind(color_module[1:2,],color_module[3:4,])%>%
+  cbind(color_module[5:6,])%>%
+  cbind(color_module[7:8,])%>%
+  cbind(color_module[9:10,])%>%
+  cbind(color_module[11:12,])%>%
+  cbind(color_module[13:14,])%>%
+  cbind(color_module[15:16,])%>%
+  t()%>%
+  as.data.frame(stringsAsFactors=FALSE)->color_module
+colnames(color_module)=c("module","Num")
+rownames(color_module)=NULL
+dplyr::filter(color_module,module != "grey")->color_module
+
+read.table("/data/liull/test_WGCNA_R/raw_module.assign.txt",header = T,as.is = TRUE) %>%
+  dplyr::filter(module != "grey")->color_gene
+
+list_sets=list()
+module_names=character()
+for (i in 1:nrow(color_module)) {
+  
+  color_module$module[i] -> module_names[i]
+  dplyr::filter(color_gene,module == module_names[i])%>%
+    dplyr::select(gene)%>%
+    as.matrix()%>%
+    as.character()%>%
+    list()->list_sets[i]
+  
+}
+names(list_sets)=module_names
+
+read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/pre_PD1_filtered_symbol_expr.txt",header = T,as.is = TRUE)->pre_PD1_expr
+
+readxl::read_excel("/data/liull/immune-checkpoint-blockade/all_metadata_available.xlsx",col_names = TRUE,sheet="SRA") %>%
+  dplyr::filter(Library_strategy=="RNA-Seq") %>%
+  dplyr::filter(Cancer=="melanoma") %>%
+  dplyr::filter(Anti_target=="anti-PD1") %>%
+  dplyr::filter(Survival_time != "NA")%>%
+  dplyr::filter(Biopsy_Time=="pre-treatment")%>%###
+  dplyr::select(Run,Response,Survival_time,Survival_status,Age,Gender) ->metadata
+
+# dplyr::filter(metadata,Response %in% c("CR","PR","R"))-> response
+# dplyr::filter(metadata,Response %in% c("SD","PD","NR")) -> non_response
+# dplyr::select(pre_PD1_expr,response$Run,non_response$Run)->ordered_all_expr
+
+
+ssgava_score <- gsva(as.matrix(pre_PD1_expr), list_sets, min.sz=1, max.sz=999999, method="ssgsea",kcdf="Gaussian")
+
+t(ssgava_score) %>%
+  as.data.frame(stringsAsFactors=FALSE)->t_ssgava_score
+cbind(rownames(t_ssgava_score),t_ssgava_score)->t_ssgava_score
+rownames(t_ssgava_score)=NULL
+colnames(t_ssgava_score)[1]="Run"
+
+Combined_data=merge(metadata[,c(1,3,4,5,6)],t_ssgava_score)
+
+for (j in 1:nrow(Combined_data)) {
+  if(Combined_data$Survival_status[j]=="Dead"){
+    Combined_data$Survival_status[j]="2"
+  }else {
+    Combined_data$Survival_status[j]="1"
+  }
+}
+
+for (j in 1:nrow(Combined_data)) {
+  if(Combined_data$Gender[j]=="female"){
+    Combined_data$Gender[j]="2"
+  }else if(Combined_data$Gender[j]=="male") {
+    Combined_data$Gender[j]="1"
+  }
+}
+
+Combined_data$Survival_time=as.numeric(Combined_data$Survival_time)
+Combined_data$Survival_status=as.numeric(Combined_data$Survival_status)
+Combined_data$Age=as.numeric(Combined_data$Age)
+Combined_data$Gender=as.numeric(Combined_data$Gender)
+
+#Univariate Cox regression
+covariates <- colnames(Combined_data)[-c(1,2,3)]
+
+univ_formulas <- sapply(covariates,
+                        function(x) as.formula(paste('Surv(Survival_time, Survival_status)~',x)))
+
+univ_models <- lapply(univ_formulas, function(x){coxph(x, data = Combined_data)})
+# Extract data 
+univ_results <- lapply(univ_models,
+                       function(x){ 
+                         x <- summary(x)
+                         p.value<-signif(x$wald["pvalue"], digits=2)
+                         wald.test<-signif(x$wald["test"], digits=2)
+                         beta<-signif(x$coef[1], digits=2);#coeficient beta
+                         HR <-signif(x$coef[2], digits=2);#exp(beta)
+                         HR.confint.lower <- signif(x$conf.int[,"lower .95"], 2)
+                         HR.confint.upper <- signif(x$conf.int[,"upper .95"],2)
+                         HR <- paste0(HR, " (", 
+                                      HR.confint.lower, "-", HR.confint.upper, ")")
+                         res<-c(beta, HR, wald.test, p.value)
+                         names(res)<-c("beta", "HR (95% CI for HR)", "wald.test", 
+                                       "p.value")
+                         return(res)
+                         #return(exp(cbind(coef(x),confint(x))))
+                       })
+
+
+res <- as.data.frame(t(as.data.frame(univ_results, check.names = FALSE)),stringsAsFactors=FALSE)
+res$p.value=as.numeric(res$p.value)
+
+
+cbind(rownames(res),res)->filter_res
+colnames(filter_res)[1]="module"
+filter_res %>%
+  dplyr::filter(p.value<=0.05)%>%
+  dplyr::select(module)->selected_modules
+# > selected_modules
+# module
+# 1   orange  0.36
+# 2     pink  0.087
+# 3    plum1  0.14
+# 4 skyblue3  0.0041
+
+Combined_data %>%
+  dplyr::select(Run,Survival_time,Survival_status,as.character(selected_modules[4,1]))%>%
+  dplyr::mutate(Class=rep("class",nrow(Combined_data)))-> Combined_module_1
+cutoff=mean(Combined_module_1[,4])
+
+for (i in 1:nrow(Combined_module_1)) {
+  if(Combined_module_1[i,4]>=cutoff){
+    Combined_module_1$Class[i]="high"
+  }else {
+    Combined_module_1$Class[i]="low"
+  }
+}
+
+fit <- survfit(Surv(Survival_time, Survival_status) ~ Class, data = Combined_module_1)
+
+pdf(file="/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/survival/modules/WGCNA/R_skyblue3_survival.pdf")
+ggsurvplot(fit, data = Combined_module_1, pval = TRUE,risk.table = TRUE,risk.table.col = "strata")
 dev.off()
-#have been removed batch effect so no need to remove samples
 
-#Choose best power-------------------------------------------------------------------------------------------------------------
-powers = c(c(1:10), seq(from = 12, to=20, by=2))
-sft = pickSoftThreshold(expr, powerVector = powers, verbose = 5)
+#
+dplyr::filter(metadata,Response %in% c("CR","PR")) -> response#
+dplyr::filter(metadata,Response %in% c("SD","PD")) -> non_response#
 
-# Plot
-jpeg('soft_threshold_power.jpeg',width = 1000, height = 500, units = "px", pointsize = 10,quality = 100,bg = "#e5ecff",res=100)
-par(mfrow = c(1,2))
-cex1 = 0.9
-plot(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2],
-     xlab="Soft Threshold (power)",ylab="Scale Free Topology Model Fit,signed R^2",type="n",
-     main = paste("Scale independence"));
-text(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2],
-     labels=powers,cex=cex1,col="red");
-# this line corresponds to using an R^2 cut-off of h
-abline(h=0.90,col="red")
+dplyr::filter(color_gene,module=="skyblue3") %>% dplyr::select(gene) %>% as.matrix() %>%as.character()->skyblue3_genes
+tibble::rownames_to_column(pre_PD1_expr) %>%
+  dplyr::filter(rowname %in% skyblue3_genes) %>%
+  dplyr::select(rowname,response$Run,non_response$Run)->skyblue3_expr
+rownames(skyblue3_expr)=skyblue3_expr$rowname
+skyblue3_expr[,-1]->skyblue3_expr
 
-# Mean connectivity as a function of the soft-thresholding power
-plot(sft$fitIndices[,1], sft$fitIndices[,5],
-     xlab="Soft Threshold (power)",ylab="Mean Connectivity", type="n",
-     main = paste("Mean connectivity"))
-text(sft$fitIndices[,1], sft$fitIndices[,5], labels=powers, cex=cex1,col="red")
-dev.off()
-#power=12
+apply(skyblue3_expr, 1, scale) ->scaled_expr
+rownames(scaled_expr)=colnames(skyblue3_expr)
+scaled_expr=t(scaled_expr)
 
 
-#Generating adjacency and TOM similarity matrices based on the selected softpower--------------------------------------------------------
-softPower = 12
-SubGeneNames<-colnames(expr)
-adjacency = adjacency(expr, power = softPower)
-TOM = TOMsimilarity(adjacency)
-dissTOM = 1-TOM  # Turn adjacency into topological overlap
-geneTree = hclust(as.dist(dissTOM), method = "average") ## Call the hierarchical clustering function
-# jpeg('TOM_based_gene_cluster.jpeg',width = 1500, height = 800, units = "px", pointsize = 12,quality = 100,bg = "#e5ecff",res=100)
-# plot(geneTree, xlab="", sub="", main = "Gene clustering on TOM-based dissimilarity",labels = FALSE, hang = 0.04)
-# dev.off()
-
-dynamicMods = cutreeDynamic(dendro = geneTree, distM = dissTOM,
-                            deepSplit = 2, pamRespectsDendro = FALSE,
-                            minClusterSize = 30);
-table(dynamicMods)
-dynamicColors = labels2colors(dynamicMods)
-table(dynamicColors)
-
-MEList = moduleEigengenes(expr, colors = dynamicColors)
-MEs = MEList$eigengenes
-# jpeg('eigengene.jpeg',width = 1000, height = 500, units = "px", pointsize = 12,quality = 100,bg = "#e5ecff",res=100)
-# plotEigengeneNetworks(MEs, "",plotDendrograms = FALSE, marHeatmap = c(2,4,1,2))
-# dev.off()
-MEDiss = 1-cor(MEs)
-MEDissThres = 0.25
-
-merge = mergeCloseModules(expr, dynamicColors, cutHeight = MEDissThres, verbose = 3)
-mergedColors = merge$colors
-mergedMEs = merge$newMEs
-
-##################save raw modules
-geneinfo<-data.frame(gene=SubGeneNames,module=dynamicColors)
-write.table(geneinfo, file = "/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/survival/modules/raw_module.assign.txt",sep="\t",quote=F,row.names=F)
-##################save merged modules
-geneinfo2<-data.frame(gene=SubGeneNames,module=mergedColors)
-write.table(geneinfo2, file = "/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/survival/modules/merged_module.assign.txt",sep="\t",quote=F,row.names=F)
-
-
-#one step
-net = blockwiseModules(expr, power = 12,
-                       TOMType = "unsigned", minModuleSize = 30,
-                       reassignThreshold = 0, mergeCutHeight = 0.25,
-                       numericLabels = TRUE, pamRespectsDendro = FALSE,
-                       saveTOMs = TRUE,
-                       saveTOMFileBase = "femaleMouseTOM",
-                       verbose = 3)
+df = data.frame(type = c(rep("response", nrow(response)), rep("non_response", nrow(non_response))))
+ha = HeatmapAnnotation(df = df,col = list(type = c("response" =  "tomato", "non_response" = "steelblue")))
+Heatmap(scaled_expr,name="Color_key",top_annotation = ha,cluster_columns = FALSE,column_names_gp = gpar(fontsize = 2),row_names_gp = gpar(fontsize = 6),col=colorRamp2(c(-3, 0, 3), c("green", "black", "red")))
--- src/09-melanoma-PD1-survival-coXpress.R
+++ src/09-melanoma-PD1-survival-coXpress.R
@@ -1,4 +1,6 @@
 library(coXpress)
+library(magrittr)
+
 
 # Step 1: reading your data
 read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/pre_PD1_filtered_symbol_expr.txt",header = T,as.is = TRUE) ->all_expr
@@ -8,27 +10,150 @@ readxl::read_excel("/data/liull/immune-checkpoint-blockade/all_metadata_availabl
   dplyr::filter(Cancer=="melanoma") %>%
   dplyr::filter(Anti_target=="anti-PD1") %>%
   dplyr::filter(Biopsy_Time=="pre-treatment")%>%
-  dplyr::select(SRA_Study,Run,Response,Biopsy_Time) ->metadata
+  dplyr::select(SRA_Study,Run,Response,Biopsy_Time,Survival_time,Survival_status,Age,Gender) ->metadata
 dplyr::filter(metadata,Response %in% c("CR","PR","R"))-> response
 dplyr::filter(metadata,Response %in% c("SD","PD","NR")) -> non_response
 dplyr::select(all_expr,response$Run,non_response$Run)->ordered_all_expr
 
+#focus on response group---------------------------------------------------------------------------------------------------------------------------
 # Step 2: Cluster data based on a subset of experiments.
-hc.gene <- cluster.gene(ordered_all_expr[,1:nrow(response)], "pearson", "average")
+hc.gene_R <- cluster.gene(ordered_all_expr[,1:nrow(response)], "pearson", "average")
 
 # Step 3: cut the tree at a height of 0.4 
-g <- cutree(hc.gene, h=0.4)
+g_R <- cutree(hc.gene_R, h=0.4)
 
 # Step 4: examine the difference between R and NR
-cox <- coXpress(ordered_all_expr, g, 1:nrow(response), (nrow(response)+1):(nrow(response)+nrow(non_response)), times=1000)
-write.table(cox,"/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/survival/modules/coXpress/all_groups.txt",quote = FALSE,col.names = TRUE)
+cox_R <- coXpress(ordered_all_expr, g_R, 1:nrow(response), (nrow(response)+1):(nrow(response)+nrow(non_response)), times=1000)
+write.table(cox_R,"all_groups_R.txt",quote = FALSE,col.names = TRUE)
 
 # Step 5: the results are a data.frame, with one row for each group of genes.
-cox[cox$pr.g1 <= .05 & cox$pr.g2 >= 0.05 & cox$N>=7,]## look for groups of genes with >= 7 members that are non-random in group 1 and random in group 2
+cox_R[cox_R$pr.g1 <= 0.05 & cox_R$pr.g2 >= 0.05 & cox_R$N>=7,]->selected_group_R
 
-# Step 6: examine groups of interest graphically     
-# look at group 239
+# Step 6: examine groups of interest graphically ,look at group 239
 plot.compare.group(ordered_all_expr,g,239,1:nrow(response), (nrow(response)+1):(nrow(response)+nrow(non_response)),
                    scale.center=TRUE,scale.scale=TRUE,
                    ylim=c(-5,5))
 inspect.group(ordered_all_expr,g,239,1:nrow(response), (nrow(response)+1):(nrow(response)+nrow(non_response)))#group 239's paied members' correlation in the R and NR
+
+
+
+#focus on non_response group---------------------------------------------------------------------------------------------------------
+hc.gene_NR <- cluster.gene(ordered_all_expr[,(nrow(response)+1):(nrow(response)+nrow(non_response))], "pearson", "average")
+g_NR <- cutree(hc.gene_NR, h=0.4)
+cox_NR <- coXpress(ordered_all_expr, g_NR, (nrow(response)+1):(nrow(response)+nrow(non_response)),1:nrow(response),  times=1000)
+write.table(cox_NR,"/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/survival/modules/coXpress/all_groups_NR.txt",quote = FALSE,col.names = TRUE)
+
+cox_NR[cox_NR$pr.g1 <= 0.05 & cox_NR$pr.g2 >= 0.05 & cox_NR$N>=7,]->selected_group_NR
+
+
+
+
+#ssGSEA--------------------
+list_sets=list()
+group_names=character()
+for (i in 1:nrow(selected_group)) {
+  
+  group=selected_group$group[i]
+  inspect.group(ordered_all_expr,g,selected_group$group[i],1:26, 27:85)->sets
+  list_sets[i]=list(union(sets$GeneA,sets$GeneB))
+  group_names[i]=paste("group",group,sep="_")
+  
+}
+names(list_sets)=group_names
+
+ssgava_score <- gsva(as.matrix(ordered_all_expr), list_sets, min.sz=1, max.sz=999999, method="ssgsea",kcdf="Gaussian", abs.ranking=FALSE, verbose=TRUE)
+
+t(ssgava_score) %>%
+  as.data.frame(stringsAsFactors=FALSE)->t_ssgava_score
+cbind(rownames(t_ssgava_score),t_ssgava_score)->t_ssgava_score
+rownames(t_ssgava_score)=NULL
+colnames(t_ssgava_score)[1]="Run"
+
+Combined_data=merge(metadata[,c(2,5,6,7,8)],t_ssgava_score)
+
+for (j in 1:nrow(Combined_data)) {
+  if(Combined_data$Survival_status[j]=="Dead"){
+    Combined_data$Survival_status[j]="2"
+  }else {
+    Combined_data$Survival_status[j]="1"
+  }
+}
+
+for (j in 1:nrow(Combined_data)) {
+  if(Combined_data$Gender[j]=="female"){
+    Combined_data$Gender[j]="2"
+  }else if(Combined_data$Gender[j]=="male") {
+    Combined_data$Gender[j]="1"
+  }
+}
+
+Combined_data$Survival_time=as.numeric(Combined_data$Survival_time)
+Combined_data$Survival_status=as.numeric(Combined_data$Survival_status)
+Combined_data$Age=as.numeric(Combined_data$Age)
+Combined_data$Gender=as.numeric(Combined_data$Gender)
+
+
+#Univariate Cox regression
+covariates <- colnames(Combined_data)[-c(1,2,3)]
+
+univ_formulas <- sapply(covariates,
+                        function(x) as.formula(paste('Surv(Survival_time, Survival_status)~',x)))
+
+univ_models <- lapply(univ_formulas, function(x){coxph(x, data = Combined_data)})
+# Extract data 
+univ_results <- lapply(univ_models,
+                       function(x){ 
+                         x <- summary(x)
+                         p.value<-signif(x$wald["pvalue"], digits=2)
+                         wald.test<-signif(x$wald["test"], digits=2)
+                         beta<-signif(x$coef[1], digits=2);#coeficient beta
+                         HR <-signif(x$coef[2], digits=2);#exp(beta)
+                         HR.confint.lower <- signif(x$conf.int[,"lower .95"], 2)
+                         HR.confint.upper <- signif(x$conf.int[,"upper .95"],2)
+                         HR <- paste0(HR, " (", 
+                                      HR.confint.lower, "-", HR.confint.upper, ")")
+                         res<-c(beta, HR, wald.test, p.value)
+                         names(res)<-c("beta", "HR (95% CI for HR)", "wald.test", 
+                                       "p.value")
+                         return(res)
+                         #return(exp(cbind(coef(x),confint(x))))
+                       })
+
+
+res <- as.data.frame(t(as.data.frame(univ_results, check.names = FALSE)),stringsAsFactors=FALSE)
+res$p.value=as.numeric(res$p.value)
+
+cbind(rownames(res),res)->filter_res
+colnames(filter_res)[1]="gene_group"
+filter_res %>%
+  dplyr::filter(p.value<=0.05)%>%
+  dplyr::select(gene_group)->groups
+# > groups
+# gene_group
+# 1  group_239
+
+Combined_data %>%
+  dplyr::select(Run,Survival_time,Survival_status,as.character(groups[,1]))->Combined_data_2
+cutoff=median(Combined_data_2$group_239)
+Combined_data_2 %>%
+  dplyr::mutate(Class=rep("class",nrow(Combined_data_2)))%>%
+  dplyr::select(Run,Survival_time,Survival_status,group_239,Class)->Combined_data_3
+
+
+
+for (i in 1:nrow(Combined_data_3)) {
+  if(Combined_data_3$group_239[i]>=cutoff){
+    Combined_data_3$Class[i]="high"
+  }else {
+    Combined_data_3$Class[i]="low"
+  }
+}
+
+fit <- survfit(Surv(Survival_time, Survival_status) ~ Class, data = Combined_data_3)
+ggsurvplot(fit, data = Combined_data_3, pval = TRUE,risk.table = TRUE,risk.table.col = "strata")
+
+
+
+
+
+
