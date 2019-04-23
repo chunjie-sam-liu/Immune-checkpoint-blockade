1317a31f716a310e1f3e1f4d0776ffd1dc5b7b52
liulanlan812 <liull_hust@hust.edu.cn>
2019-04-23 20:02:42 +0800

    single test

--- src/06-differential-test_pipeline_SPR070710.R
+++ src/06-differential-test_pipeline_SPR070710.R
@@ -4,99 +4,66 @@ library(magrittr)
 library(NOISeq)
 readxl::read_excel("/data/liull/immune-checkpoint-blockade/all_metadata_available.xlsx",col_names = TRUE,sheet="SRA") %>%
   dplyr::filter(SRA_Study=="SRP070710") %>%
-  dplyr::filter(Biopsy_Time=="pre-treatment")%>%
+  #dplyr::filter(Biopsy_Time=="pre-treatment")%>%
   dplyr::select(Run,Response) ->metadata
-#filter SRP070710  metadata
+
 dplyr::filter(metadata,Response %in% c("CR","PR")) ->response
 dplyr::filter(metadata,Response=="PD") ->non_response
 
 #select response and non_response
-
+read.table("/data/liull/reference/EntrezID_Symbl_EnsemblID_NCBI.txt",sep="\t",header = T,as.is = TRUE) ->relationship
 read.table("/data/liull/immune-checkpoint-blockade/expression/all_FPKM_expression_2.txt",sep="\t",header = T,as.is = TRUE) ->data1
 
-dplyr::select(data1,gene_id,response$Run,non_response$Run)->all_expression
-rownames(all_expression)=all_expression[,1]
-all_expression=all_expression[,-1]
+data1%>%
+  dplyr::select(gene_id,response$Run,non_response$Run)%>%
+  dplyr::filter(gene_id %in% grep("ENSG",data1$gene_id,value=T))->SRP070710_expr
+# %>%
+#   dplyr::filter(gene_id %in% relationship$Ensembl_ID) %>%
+#   merge(relationship,.,by.x="Ensembl_ID",by.y="gene_id")%>%
+#   dplyr::select(-Ensembl_ID,-GeneID)->SRP070710_expr  #[1] 21003    29
 
+# factors=factor(SRP070710_expr$Symbol)
+# merged_expression=tapply(SRP070710_expr[,2],factors,median)
+# for (i in 3:ncol(SRP070710_expr)) {
+#   temp=tapply(SRP070710_expr[,i],factors,median)
+#   merged_expression=cbind(merged_expression,temp)
+# }
+# colnames(merged_expression)=colnames(SRP070710_expr)[2:ncol(SRP070710_expr)]  #trans ensembl id to symbol and merged
+# dim(merged_expression)#[1] 20986    28
 
-all_expression[is.na(all_expression)]<-0
-# Sum_zero=apply(all_expression,1,function(x) sum(as.numeric(x),na.rm = TRUE))
-# IDs_zero=which(Sum_zero==0)
-# all_expression_2=all_expression[-IDs_zero,]#delete the gene has all 0.000 expression
+# keep <- rowSums(merged_expression>3,na.rm = TRUE) > 0
+# merged_expression[keep,]->filtered_expr  #[1] 11165    28
+rownames(SRP070710_expr)=SRP070710_expr$gene_id
+SRP070710_expr=SRP070710_expr[,-1]
+SRP070710_expr[is.na(SRP070710_expr)]<-0
 
+median_R=apply(SRP070710_expr, 1, function(x) median(x[1:15]))
+median_NR=apply(SRP070710_expr, 1, function(x) median(x[16:28]))
+diff_median=apply(SRP070710_expr, 1, function(x) (median(x[1:15])+0.001)/(median(x[16:28])+0.001))
+p.val=apply(SRP070710_expr, 1, function(x) wilcox.test(x[1:15],x[16:28],alternative="two.sided")$p.value)
 
-b=nrow(response)
-c=nrow(response)+1
-d=ncol(all_expression)
-Pvalue<-c(rep(0,nrow(all_expression)))
-FC<-c(rep(0,nrow(all_expression)))
-for(i in 1:nrow(all_expression)){
-  
-  if(sd(all_expression[i,1:b],na.rm = TRUE)==0){
-    Pvalue[i] <- "NA"
-    FC[i]<- "NA"
-  }else{
-    y=wilcox.test(as.numeric(all_expression[i,1:b]),as.numeric(all_expression[i,c:d]),alternative = "two.sided")
-    Pvalue[i]<-y$p.value
-    FC[i]<-median(as.numeric(all_expression[i,1:b]))-median(as.numeric(all_expression[i,c:d]))
-  }
+cbind(median_R,median_NR,diff_median,p.val)%>%
+  as.data.frame(stringsAsFactors=FALSE)->result
+cbind(rownames(SRP070710_expr),result,stringsAsFactors=FALSE)->result
 
-}
-
-avg_response=apply(all_expression[,1:b],1,median)
-avg_nonresponse=apply(all_expression[,c:d],1,median)
-
-out<-cbind(all_expression,avg_response,avg_nonresponse,Pvalue,FC)
-out$Pvalue=as.numeric(as.character(out$Pvalue))
-out$FC=as.numeric(as.character(out$FC))
-# > dim(out)
-# [1] 49325    31
-
-del=which(is.na(out$Pvalue)==TRUE)
-out2=out[-del,]#delete the Pvalue is NA 
-# > dim(out2)
-# [1] 44128    31
-
-out2=cbind(rownames(out2),out2)
-rownames(out2)=NULL
-colnames(out2)[1]="gene_id"
-out2$gene_id <- as.character(out2$gene_id)#change rownames to out[,1]
-#write.table(out2,"/data/liull/immune-checkpoint-blockade/different_expression/test_SRP070710/all_diff_expression.txt",quote = FALSE,sep="\t",row.names = FALSE,col.names = TRUE)
-
-dplyr::filter(out2,Pvalue<=0.1) -> result
-dplyr::filter(result,FC>=1) -> up_in_response
-dplyr::filter(result,FC<= -1) -> down_in_response
-# write.table(up_in_response,"/data/liull/immune-checkpoint-blockade/different_expression/test_SRP070710/up_0.1_2.txt",quote = FALSE,sep="\t",row.names = FALSE,col.names = TRUE)
-# write.table(down_in_response,"/data/liull/immune-checkpoint-blockade/different_expression/test_SRP070710/down_0.1_2.txt",quote = FALSE,sep="\t",row.names = FALSE,col.names = TRUE)
-
+dplyr::filter(result,p.val<0.1)%>%
+  dplyr::filter(diff_median>2)->up
+dplyr::filter(result,p.val<0.1)%>%
+  dplyr::filter(diff_median< 0.5)->down
 
 
 readxl::read_excel("/data/liull/immune-checkpoint-blockade/different_expression/test_SRP070710/1-s2.0-S009286741630215X-mmc2.xlsx",col_names = TRUE,sheet="S2A",skip = 2) ->standard_diff_genes
-standard_up=dplyr::filter(standard_diff_genes,diffAvg>0)
-standard_down=dplyr::filter(standard_diff_genes,diffAvg<0)
+dplyr::filter(standard_diff_genes,diffAvg>0)%>%
+  merge(relationship,.,by.x="Symbol",by.y="Gene")%>%
+  dplyr::select(Ensembl_ID)->standard_up
 
-read.table("/data/liull/reference/EntrezID_Symbl_EnsemblID_NCBI_2.txt",sep="\t",header = T,as.is = TRUE) ->relationship
+standard_down=dplyr::filter(standard_diff_genes,diffAvg<0)%>%
+  merge(relationship,.,by.x="Symbol",by.y="Gene")%>%
+  dplyr::select(Ensembl_ID)->standard_down
 
-dplyr::filter(up_in_response,gene_id %in% relationship$EnsemblId) %>%
-  merge(relationship,.,by.x="EnsemblId",by.y="gene_id")%>%
-  dplyr::select(Symbol)->up_in_response_symbol
-length(dplyr::intersect(standard_up$Gene,up_in_response_symbol$Symbol))#30
-
-dplyr::filter(down_in_response,gene_id %in% relationship$EnsemblId) %>%
-  merge(relationship,.,by.x="EnsemblId",by.y="gene_id")%>%
-  dplyr::select(Symbol)->down_in_response_symbol
-length(dplyr::intersect(standard_down$Gene,down_in_response_symbol$Symbol))#76
-
-
-
-# ID_up=as.character(as.data.frame(standard_up[,1])$Gene)
-# ID_down=as.character(as.data.frame(standard_down[,1])$Gene)
-# read.table("/data/liull/salmon_tutorial/Homo_quants/gene_transcript_relationship.txt",header = T) -> relationship
-# list=relationship[,c(1,3)]
-# ID_list_up=list[match(ID_up,list[,"gene_name"]),]#从gene_transcript_relationship.txt中提取出up的基因的ensembl id。
-# ID_list_down=list[match(ID_down,list[,"gene_name"]),]#从gene_transcript_relationship.txt中提取出down的基因的ensembl id。
-# 
-# length(dplyr::intersect(ID_list_up$gene_id,up_in_response$gene_id))
-# length(dplyr::intersect(ID_list_down$gene_id,down_in_response$gene_id))
-
+length(intersect(standard_up$Ensembl_ID,up$`rownames(SRP070710_expr)`))
+# median(R)-median(NR): 30  FC:59
+length(intersect(standard_down$Ensembl_ID,down$`rownames(SRP070710_expr)`))
+# median(R)-median(NR): 94  FC:179
+ 
 
--- src/06-melanoma_PD1_single_project_test.R
+++ src/06-melanoma_PD1_single_project_test.R
@@ -61,8 +61,10 @@ output <- topTable(fit2, coef=2, n=Inf)
 tibble::rownames_to_column(output) %>% dplyr::filter(P.Value<0.1) %>% dplyr::filter(logFC>log2(1.5))->up#372
 tibble::rownames_to_column(output) %>% dplyr::filter(P.Value<0.1) %>% dplyr::filter(logFC<log2(1/1.5))->down#1072
 
-write.table(up,paste("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/single_test/SRP070710_all_up.txt",sep="" ),quote = FALSE,row.names = FALSE,col.names = TRUE)
-write.table(down,paste("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/single_test/SRP070710_all_down.txt",sep="" ),quote = FALSE,row.names = FALSE,col.names = TRUE)
+write.table(up,"/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/single_test/SRP070710_all_up.txt",
+            sep="",quote = FALSE,row.names = FALSE,col.names = TRUE)
+write.table(down,"/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/single_test/SRP070710_all_down.txt",
+            quote = FALSE,row.names = FALSE,col.names = TRUE)
 
 
 #up
@@ -85,6 +87,8 @@ venn.diagram(list(SRP070710=down$rowname, SRP070710_standard=SRP070710_standard_
              filename = "/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/single_test/SRP070710_venn_down.png",
              fill=c("red","yellow"),na="remove")
 
+#SRP150548's paper no DEG--------------
+
 
 #SRP094781_standard vs SRP094781_me -------------------------------------------
 i=3
@@ -93,17 +97,29 @@ dplyr::filter(metadata,SRA_Study==Project[i])%>%
 dplyr::filter(traits,Response %in% c("CR","PR")) ->response
 dplyr::filter(traits,Response %in% c("PD","SD")) ->non_response
 
-dplyr::select(all_count_data,gene_id,response$Run,non_response$Run)->single_expr
-row.names(single_expr)=single_expr[,1]
-single_expr=single_expr[,-1]
+all_count_data%>%
+  dplyr::select(gene_id,response$Run,non_response$Run)%>%
+  dplyr::filter(gene_id %in% relationship$Ensembl_ID) %>%
+  merge(relationship,.,by.x="Ensembl_ID",by.y="gene_id")%>%
+  dplyr::select(-Ensembl_ID,-GeneID)->single_expr  #symbol-PD1-pretreatment-log2CPM-expr
+dim(single_expr)#[1] 22114    50
 
-DGEList_expr <- DGEList(counts=single_expr)
+factors=factor(single_expr$Symbol)
+merged_expression=tapply(single_expr[,2],factors,median)
+for (i in 3:ncol(single_expr)) {
+  temp=tapply(single_expr[,i],factors,median)
+  merged_expression=cbind(merged_expression,temp)
+}
+colnames(merged_expression)=colnames(single_expr)[2:ncol(single_expr)]  #trans ensembl id to symbol and merged
+dim(merged_expression)#[1] 22094    49
+
+DGEList_expr <- DGEList(counts=merged_expression)
 normalized_expr <- calcNormFactors(DGEList_expr, method="upperquartile")
 normalized_loggedCPM_expr = cpm(normalized_expr, log=TRUE, prior.count=2)
 
 keep <- rowSums(normalized_loggedCPM_expr>0) >= 2
 normalized_loggedCPM_expr <- normalized_loggedCPM_expr[keep,]
-dim(normalized_loggedCPM_expr)
+dim(normalized_loggedCPM_expr)#[1] 16348    49
 #delete the gene has less than 2 sample exression CPM<1(log2CPM<0)
 
 group_list <- factor(c(rep("response",nrow(response)), rep("non_response",nrow(non_response))))
@@ -118,31 +134,20 @@ output <- topTable(fit2, coef=2, n=Inf)
 tibble::rownames_to_column(output) %>% dplyr::filter(P.Value<0.1) %>% dplyr::filter(logFC>log2(1.2))->up
 tibble::rownames_to_column(output) %>% dplyr::filter(P.Value<0.1) %>% dplyr::filter(logFC<log2(1/1.2))->down
 
-merge(relationship,up,by.x="Ensembl_ID",by.y="rowname",all=TRUE)%>%
-  dplyr::filter(Ensembl_ID %in% up$rowname) ->up2
-up2[order(up2$logFC,decreasing = TRUE),]->up2
-merge(relationship,down,by.x="Ensembl_ID",by.y="rowname",all=TRUE)%>%
-  dplyr::filter(Ensembl_ID %in% down$rowname) ->down2
-down2[order(down2$logFC),]->down2
-dim(up2)
-dim(down2)
-write.table(up2,paste("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/single_test/",Project[i],"_all_up_1.2.txt",sep="" ),quote = FALSE,row.names = FALSE,col.names = TRUE)
-write.table(down2,paste("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/single_test/",Project[i],"_all_down_1.2.txt",sep="" ),quote = FALSE,row.names = FALSE,col.names = TRUE)
+write.table(up,"/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/single_test/SRP094781_all_up.txt",
+            quote = FALSE,row.names = FALSE,col.names = TRUE)
+write.table(down,"/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/single_test/SRP094781_all_down.txt",
+            quote = FALSE,row.names = FALSE,col.names = TRUE)
 
-read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/single_test/SRP094781_all_up_1.2.txt",header = T,as.is = TRUE) ->SRP094781_up
-read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/single_test/SRP094781_all_down_1.2.txt",header = T,as.is = TRUE) ->SRP094781_down
 #up
 read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/single_test/SRP094781_standard_up.txt",sep="\t",header = T,as.is = TRUE)%>%
   as.matrix()%>%
   as.character()->SRP094781_standard_up
 
-dplyr::filter(SRP094781_up,Ensembl_ID %in% grep("ENSG",SRP094781_up$Ensembl_ID,value=T))%>%
-  dplyr::select(Symbol)%>%
-  as.matrix()%>%
-  as.character()->SRP094781_me_up
-
-setwd("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/single_test/")
-venn.diagram(list(SRP094781=SRP094781_me_up, SRP094781_standard=SRP094781_standard_up),main="SRP094781 VS SRP094781_standard: up",cex=2,margin = 0.1,imagetype = "png",filename="SRP094781_venn_up.png",fill=c("green","yellow"),na="remove")
+venn.diagram(list(SRP094781=up$rowname, SRP094781_standard=SRP094781_standard_up),main="SRP094781 VS SRP094781_standard: up",
+             cex=2,margin = 0.1,imagetype = "png",
+             filename="/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/single_test/SRP094781_venn_up.png",
+             fill=c("green","yellow"),na="remove")
 
 
 #down
@@ -150,31 +155,30 @@ read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/mel
   as.matrix()%>%
   as.character()->SRP094781_standard_down
 
-dplyr::filter(SRP094781_down,Ensembl_ID %in% grep("ENSG",SRP094781_down$Ensembl_ID,value=T))%>%
-  dplyr::select(Symbol)%>%
-  as.matrix()%>%
-  as.character()->SRP094781_me_down
-
-venn.diagram(list(SRP094781=SRP094781_me_down, SRP094781_standard=SRP094781_standard_down),main="SRP094781 VS SRP094781_standard: down",cex=2,margin = 0.1,imagetype = "png",filename="SRP094781_venn_down.png",fill=c("green","yellow"),na="remove")
+venn.diagram(list(SRP094781=down$rowname, SRP094781_standard=SRP094781_standard_down),main="SRP094781 VS SRP094781_standard: down",
+             cex=2,margin = 0.1,imagetype = "png",
+             filename="/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/single_test/SRP094781_venn_down.png",
+             fill=c("green","yellow"),na="remove")
 
 
-#SRP150548's paper no DEG--------------
+
 
 
 
 
 #single result compare with all_together-------------------------------------------------
-read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/single_test/SRP070710_all_up_1.5.txt",header = T,as.is = TRUE) ->SRP070710_up
-read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/single_test/SRP070710_all_down_1.5.txt",header = T,as.is = TRUE) ->SRP070710_down
-read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/single_test/SRP094781_all_up_1.5.txt",header = T,as.is = TRUE) ->SRP094781_up
-read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/single_test/SRP094781_all_down_1.5.txt",header = T,as.is = TRUE) ->SRP094781_down
+read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/single_test/SRP070710_all_up.txt",header = T,as.is = TRUE) ->SRP070710_up
+read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/single_test/SRP070710_all_down.txt",header = T,as.is = TRUE) ->SRP070710_down
+read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/single_test/SRP094781_all_up.txt",header = T,as.is = TRUE) ->SRP094781_up
+read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/single_test/SRP094781_all_down.txt",header = T,as.is = TRUE) ->SRP094781_down
 read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/single_test/SRP150548_all_up_1.5.txt",header = T,as.is = TRUE) ->SRP150548_up
 read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/single_test/SRP150548_all_down_1.5.txt",header = T,as.is = TRUE) ->SRP150548_down
 
-read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/PD1_all_up.txt",header = T,as.is = TRUE) ->PD1_all_up
-read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/PD1_all_down.txt",header = T,as.is = TRUE) ->PD1_all_down
+read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/PD1_up_ENSG.txt",header = T,as.is = TRUE) ->PD1_all_up
+read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/PD1_down_ENSG.txt",header = T,as.is = TRUE) ->PD1_all_down
 
-venn.diagram(list(SRP070710=SRP070710_up$Ensembl_ID, SRP094781=SRP094781_up$Ensembl_ID,SRP150548=SRP150548_up$Ensembl_ID,PD1_all=PD1_all_up$rowname),filename=NULL,fill=c("cornflowerblue", "green", "yellow","red"))->venn_up
+venn.diagram(list(SRP070710=SRP070710_up$rowname, SRP094781=SRP094781_up$rowname,SRP150548=SRP150548_up$Symbol,PD1_all=PD1_all_up$Symbol),
+             filename=NULL,fill=c("cornflowerblue", "green", "yellow","red"),na="remove")->venn_up
 grid.draw(venn_up)
 ggsave(
   filename = 'venn_up_interaction.pdf',
