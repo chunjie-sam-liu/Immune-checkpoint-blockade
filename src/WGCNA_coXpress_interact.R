e75d554a95a4f20cc8cd1be69a89b00712efd7de
liulanlan812 <liull_hust@hust.edu.cn>
2019-04-25 08:48:39 +0800

    interact WGCNA_coXpress

--- src/WGCNA_coXpress_interact.R
+++ src/WGCNA_coXpress_interact.R
@@ -1,4 +1,5 @@
 library(magrittr)
+#WGCNA-----------------------------------------------------------------------------------------------------------------------------------
 read.table("/data/liull/test_WGCNA_R/module.color.txt",header = F,as.is = TRUE,skip = 1) ->R_color_module #response modules
 cbind(R_color_module[1:2,],R_color_module[3:4,])%>%
   cbind(R_color_module[5:6,])%>%
@@ -68,19 +69,20 @@ names(NR_list_sets)=paste("NR_",NR_module_names,sep="")
 
 list_sets_WGCNA=c(R_list_sets,NR_list_sets)
 
+#coxpress------------------------------------------------------------------------------------------------------------------------------------------------------------
+read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/pre_PD1_filtered_symbol_expr.txt",header = T,as.is = TRUE) ->pre_PD1_expr
+readxl::read_excel("/data/liull/immune-checkpoint-blockade/all_metadata_available.xlsx",col_names = TRUE,sheet="SRA") %>%
+  dplyr::filter(Library_strategy=="RNA-Seq") %>%
+  dplyr::filter(Cancer=="melanoma") %>%
+  dplyr::filter(Anti_target=="anti-PD1") %>%
+  dplyr::filter(Biopsy_Time=="pre-treatment")%>%
+  dplyr::select(SRA_Study,Run,Response,Biopsy_Time,Survival_time,Survival_status,Age,Gender) ->metadata
+dplyr::filter(metadata,Response %in% c("CR","PR","R"))-> response#26
+dplyr::filter(metadata,Response %in% c("SD","PD","NR")) -> non_response#59
+dplyr::select(pre_PD1_expr,response$Run,non_response$Run)->ordered_PD1_expr
+read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/survival/modules/coXpress/selected_groups.txt",header = T,as.is = TRUE)->selected_groups
 
-
-read.table("/data/liull/test_coXpress/all_groups_R.txt",header = T,as.is = TRUE)%>%
-  dplyr::filter(N>8)->all_groups
-all_groups%>%
-  dplyr::filter(pr.g1<=0.05)%>%
-  dplyr::filter(pr.g2>=0.05)->selected_group_R#7
-all_groups%>%
-  dplyr::filter(pr.g1>0.05)%>%
-  dplyr::filter(pr.g2<0.05)->selected_group_NR#0
-rbind(selected_group_R,selected_group_NR)->selected_groups
-
-read.table("/data/liull/test_coXpress/g_R.txt",header = F,as.is = TRUE) ->g_dataframe
+read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/survival/modules/coXpress/g.txt",header = F,as.is = TRUE) ->g_dataframe
 g=g_dataframe$V2
 names(g)=g_dataframe$V1
 
@@ -99,21 +101,19 @@ names(list_sets_coXpress)=module_names
 
 for (i in 1:length(list_sets_WGCNA)) {
   for (j in 1:length(list_sets_coXpress)) {
+    Interaction=intersect(list_sets_WGCNA[[i]],list_sets_coXpress[[j]])
+    # length(intersect(list_sets_WGCNA[[i]],list_sets_coXpress[[j]]))/length(list_sets_WGCNA[[i]])->ratio1
+    # length(intersect(list_sets_WGCNA[[i]],list_sets_coXpress[[j]]))/length(list_sets_coXpress[[j]])->ratio2
+    if(length(Interaction)!=0){
+      print(Interaction)
     
-    length(intersect(list_sets_WGCNA[[i]],list_sets_coXpress[[j]]))/length(list_sets_WGCNA[[i]])->ratio1
-    length(intersect(list_sets_WGCNA[[i]],list_sets_coXpress[[j]]))/length(list_sets_coXpress[[j]])->ratio2
-    if(ratio1!=0 && ratio2!=0){
-      print(i)
-      print(j)
-      
-    }else{
-      #print(paste(ratio1,ratio2,sep = "-"))
     }
-    
+     
   }
 }
 
 
 # [1] 35
 # [1] 4
-# 1 interaction
+# 1 interaction:"ABCG5"
+
