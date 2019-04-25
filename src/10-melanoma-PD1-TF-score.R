7c455330c8d84612a85d0b53e78669bcf363f62f
liulanlan812 <liull_hust@hust.edu.cn>
2019-04-25 08:47:58 +0800

    TF module

--- src/10-melanoma-PD1-TF-score.R
+++ src/10-melanoma-PD1-TF-score.R
@@ -1,3 +1,6 @@
+library(magrittr)
+
+#filter the TF associated with survival with Univariate Cox regression------------------------------------------------------------------------------------------
 read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/score_with_TF/Homo_sapiens_TF",header = T,as.is = TRUE,sep="\t")%>%
   dplyr::select(Symbol)->human_TF
 
@@ -82,3 +85,80 @@ filter_res %>%
   dplyr::filter(p.value<=0.2)%>%
   dplyr::select(gene_symbol)->TF_symbol#285
 write.table(TF_symbol,"/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/score_with_TF/TF_symbol.txt",quote = FALSE,col.names = TRUE,row.names = FALSE)
+
+#TF-target in modules----------------------------------------------------------------------------------------------------------------------------------------------------------------
+read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/score_with_TF/TF_symbol.txt",header = T,as.is = TRUE)->TF_symbol
+read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/survival/modules/WGCNA/NR_white.txt",header = F,as.is = TRUE)->NR_white#0 interaction
+colnames(NR_white)="module_genes"
+read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/survival/modules/WGCNA/NR_orangered4.txt",header = F,as.is = TRUE)->NR_orangered4#0 TF
+colnames(NR_orangered4)="module_genes"
+read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/survival/modules/WGCNA/NR_midnightblue.txt",header = F,as.is = TRUE)->NR_midnightblue#*****
+colnames(NR_midnightblue)="module_genes"
+read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/survival/modules/WGCNA/R_skyblue3.txt",header = F,as.is = TRUE)->R_skyblue3#0 interaction
+colnames(R_skyblue3)="module_genes"
+
+
+read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/score_with_TF/chip_tf_to_coding_gene.txt",header = T,sep="\t",as.is = TRUE)->TF_targets
+
+dplyr::filter(TF_symbol,gene_symbol %in% NR_midnightblue$module_genes)->TFs_in_module
+dim(TFs_in_module)
+dplyr::mutate(NR_midnightblue,target=rep("NA",nrow(NR_midnightblue)))->NR_midnightblue
+
+Targets_relationship=data.frame()
+colnames(Targets_relationship)=c("TF","Targets")
+for (i in 1:nrow(TFs_in_module)) {
+  
+  every_TF=TFs_in_module$gene_symbol[i]
+  print(every_TF)
+  dplyr::filter(TF_targets,TF == every_TF)%>%
+    dplyr::select(target)%>%
+    as.matrix()%>%
+    as.character()->a
+  dplyr::filter(TF_targets,target == every_TF)%>%
+    dplyr::select(TF)%>%
+    as.matrix()%>%
+    as.character()->b
+  union(a,b)%>%unique()->every_TF_targets
+  intersect(every_TF_targets,NR_midnightblue$module_genes)->targets_in_module
+  data.frame(TF=rep(every_TF,length(targets_in_module)),Targets=targets_in_module) %>%
+    rbind(Targets_relationship) ->Targets_relationship
+  # intersect(every_TF_targets,NR_midnightblue$module_genes)->m
+  # print(m)
+  
+}
+
+# [1] 3
+# [1] 9
+# [1] 1
+# [1] 2
+
+# > Targets_relationship
+# TF Targets
+# 1  ZNF623   RAD21
+# 2  ZNF623  ZBTB10
+# 3    ZHX1   RAD21
+# 4  ZBTB10  VCPIP1
+# 5  ZBTB10   RMDN1
+# 6  ZBTB10  TMEM67
+# 7  ZBTB10  RAD54B
+# 8  ZBTB10  VPS13B
+# 9  ZBTB10   YWHAZ
+# 10 ZBTB10  TMEM65
+# 11 ZBTB10  ZNF623
+# 12 ZBTB10   RAD21
+# 13  TERF1   MTFR1
+# 14  TERF1  VPS13B
+# 15  TERF1   RAD21
+
+
+
+
+
+
+
+
+
+
+
+
+
