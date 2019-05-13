546fdf0e742c01c856acf87ac2354616b570d260
liulanlan812 <liull_hust@hust.edu.cn>
2019-05-13 19:59:50 +0800

    TIL

---
+++ src/12-melanoma-PD1-TIL.R
@@ -0,0 +1,72 @@
+library(magrittr)
+
+#melanoma PD1 TIL
+read.table("/data/miaoyr/immune_checkpoint/RNAseq_TIL_result/melanoma_log2_til",sep="\t",header = T) ->melanoma_PD1_TIL
+
+
+readxl::read_excel("/data/liull/immune-checkpoint-blockade/all_metadata_available.xlsx",col_names = TRUE,sheet="SRA") %>%
+  dplyr::filter(Cancer=="melanoma")%>%
+  dplyr::filter(Anti_target=="anti-PD1")%>%
+  dplyr::filter(Library_strategy=="RNA-Seq") %>%
+  dplyr::filter(Biopsy_Time=="pre-treatment")%>%
+  dplyr::filter(Survival_time != "NA")%>%
+  dplyr::select(Run,Response,Survival_time,Survival_status,Age,Gender) ->metadata_survival
+
+
+tibble::rownames_to_column(melanoma_PD1_TIL)%>%
+  merge(metadata_survival,.,by.x="Run",by.y="rowname")->Combined_data
+
+# coxph(Surv(Survival_time, Survival_status)~InfiltrationScore, data = Combined_data)
+
+for (j in 1:nrow(Combined_data)) {
+  if(Combined_data$Survival_status[j]=="Dead"){
+    Combined_data$Survival_status[j]="2"
+  }else {
+    Combined_data$Survival_status[j]="1"
+  }
+  if(Combined_data$Gender[j]=="female"){
+    Combined_data$Gender[j]="2"
+  }else if(Combined_data$Gender[j]=="male") {
+    Combined_data$Gender[j]="1"
+  }
+}
+Combined_data$Survival_time=as.numeric(Combined_data$Survival_time)
+Combined_data$Survival_status=as.numeric(Combined_data$Survival_status)
+Combined_data$Age=as.numeric(Combined_data$Age)
+Combined_data$Gender=as.numeric(Combined_data$Gender)
+
+#Univariate Cox regression
+covariates <- colnames(Combined_data)[-c(1,2,3,4)]
+
+univ_formulas <- sapply(covariates,
+                        function(x) as.formula(paste('Surv(Survival_time, Survival_status)~',x)))
+
+univ_models <- lapply(univ_formulas, function(x){coxph(x, data = Combined_data)})
+# Extract data 
+univ_results <- lapply(univ_models,
+                       function(x){ 
+                         x <- summary(x)
+                         p.value<-signif(x$wald["pvalue"], digits=2)
+                         wald.test<-signif(x$wald["test"], digits=2)
+                         beta<-signif(x$coef[1], digits=2);#coeficient beta
+                         HR <-signif(x$coef[2], digits=2);#exp(beta)
+                         HR.confint.lower <- signif(x$conf.int[,"lower .95"], 2)
+                         HR.confint.upper <- signif(x$conf.int[,"upper .95"],2)
+                         HR <- paste0(HR, " (", 
+                                      HR.confint.lower, "-", HR.confint.upper, ")")
+                         res<-c(beta, HR, wald.test, p.value)
+                         names(res)<-c("beta", "HR (95% CI for HR)", "wald.test", 
+                                       "p.value")
+                         return(res)
+                         #return(exp(cbind(coef(x),confint(x))))
+                       })
+
+
+res <- as.data.frame(t(as.data.frame(univ_results, check.names = FALSE)),stringsAsFactors=FALSE)
+res$p.value=as.numeric(res$p.value)
+
+cbind(rownames(res),res)->filter_res
+colnames(filter_res)[1]="TIL"
+filter_res %>%
+  dplyr::filter(p.value<=0.05)%>%
+  dplyr::select(TIL)->TIL #0
