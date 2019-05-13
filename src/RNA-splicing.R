5234733cc9a50b8b1bf062048e0e78d94e1aab5c
liulanlan812 <liull_hust@hust.edu.cn>
2019-05-13 20:00:25 +0800

    RNA-splicing

--- src/RNA-splicing.R
+++ src/RNA-splicing.R
@@ -1,55 +1,101 @@
 #RNA-splicing melanoma PD1
 library (VennDiagram)
 
-read.table("/data/liull/immune-checkpoint-blockade/RNA_splicing/SRP070710/A3SS.MATS.JCEC.txt",header = T) -> SRP070710_A3SS_JCEC
-SRP070710_A3SS_JCEC %>%
+
+#3'-----------------------------
+read.table("/data/liull/immune-checkpoint-blockade/RNA_splicing/SRP070710/A3SS.MATS.JCEC.txt",header = T) %>%
   dplyr::filter(PValue <=0.05)%>%
   dplyr::filter(abs(IncLevelDifference) >= 0.05) ->SRP070710_sig_rna
 
-read.table("/data/liull/immune-checkpoint-blockade/RNA_splicing/SRP094781/A3SS.MATS.JCEC.txt",header = T) -> SRP094781_A3SS_JCEC
-SRP094781_A3SS_JCEC %>%
+read.table("/data/liull/immune-checkpoint-blockade/RNA_splicing/SRP094781/A3SS.MATS.JCEC.txt",header = T)  %>%
   dplyr::filter(PValue <=0.05)%>%
   dplyr::filter(abs(IncLevelDifference) >= 0.05) ->SRP094781_sig_rna
 
-read.table("/data/liull/immune-checkpoint-blockade/RNA_splicing/SRP150548/A3SS.MATS.JCEC.txt",header = T) -> SRP150548_A3SS_JCEC
-SRP150548_A3SS_JCEC %>%
+read.table("/data/liull/immune-checkpoint-blockade/RNA_splicing/SRP150548/A3SS.MATS.JCEC.txt",header = T)  %>%
   dplyr::filter(PValue <=0.05)%>%
   dplyr::filter(abs(IncLevelDifference) >= 0.05) ->SRP150548_sig_rna
+#"OSGEPL1"  "TOR1AIP1" "RHOT2"    "RPAIN"
 
-read.table("/data/liull/immune-checkpoint-blockade/RNA_splicing/ERP107734/A3SS.MATS.JCEC.txt",header = T) -> ERP107734_A3SS_JCEC
-ERP107734_A3SS_JCEC %>%
+#5'-----------------------------
+read.table("/data/liull/immune-checkpoint-blockade/RNA_splicing/SRP070710/A5SS.MATS.JCEC.txt",header = T)  %>%
   dplyr::filter(PValue <=0.05)%>%
-  dplyr::filter(abs(IncLevelDifference) >= 0.05) ->ERP107734_sig_rna
+  dplyr::filter(abs(IncLevelDifference) >= 0.05) ->SRP070710_sig_rna
 
-# intersect(SRP070710_sig_rna$geneSymbol,SRP094781_sig_rna$geneSymbol)%>%
-#   intersect(SRP150548_sig_rna$geneSymbol)
-venn.diagram(list(SRP070710=SRP070710_sig_rna$geneSymbol, SRP094781=SRP094781_sig_rna$geneSymbol,
-                  SRP150548=SRP150548_sig_rna$geneSymbol),filename=NULL,fill = c("cornflowerblue", "green", "yellow"))->venn_melanoma_PD1
-grid.draw(venn_melanoma_PD1)
+read.table("/data/liull/immune-checkpoint-blockade/RNA_splicing/SRP094781/A5SS.MATS.JCEC.txt",header = T)  %>%
+  dplyr::filter(PValue <=0.05)%>%
+  dplyr::filter(abs(IncLevelDifference) >= 0.05) ->SRP094781_sig_rna
 
-# intersect(SRP070710_sig_rna$geneSymbol,SRP094781_sig_rna$geneSymbol)->a
-# intersect(SRP070710_sig_rna$geneSymbol,SRP150548_sig_rna$geneSymbol)->b
-# intersect(SRP094781_sig_rna$geneSymbol,SRP150548_sig_rna$geneSymbol)->c
-# union(a,b)%>%
-#   union(c)->all_genes
+read.table("/data/liull/immune-checkpoint-blockade/RNA_splicing/SRP150548/A5SS.MATS.JCEC.txt",header = T)  %>%
+  dplyr::filter(PValue <=0.05)%>%
+  dplyr::filter(abs(IncLevelDifference) >= 0.05) ->SRP150548_sig_rna
+#"ACTL6A"  "HYI"     "ARL6IP4" "ULK3" 
+
+#MXE-----------------------------
+read.table("/data/liull/immune-checkpoint-blockade/RNA_splicing/SRP070710/MXE.MATS.JCEC.txt",header = T)  %>%
+  dplyr::filter(PValue <=0.05)%>%
+  dplyr::filter(abs(IncLevelDifference) >= 0.05) ->SRP070710_sig_rna
+
+read.table("/data/liull/immune-checkpoint-blockade/RNA_splicing/SRP094781/MXE.MATS.JCEC.txt",header = T)  %>%
+  dplyr::filter(PValue <=0.05)%>%
+  dplyr::filter(abs(IncLevelDifference) >= 0.05) ->SRP094781_sig_rna
+
+read.table("/data/liull/immune-checkpoint-blockade/RNA_splicing/SRP150548/MXE.MATS.JCEC.txt",header = T)  %>%
+  dplyr::filter(PValue <=0.05)%>%
+  dplyr::filter(abs(IncLevelDifference) >= 0.05) ->SRP150548_sig_rna
+#[1] "MCUB"    "NDUFS1"  "PPOX"    "ZMYND8"  "SLC37A3" "GUSB"    "GIPC1"   "CERS4"   "SUN1"    "HDAC8"   "RALY"    "ATG4B"   "PPIL3"
+
+#RI-----------------------------
+read.table("/data/liull/immune-checkpoint-blockade/RNA_splicing/SRP070710/RI.MATS.JCEC.txt",header = T)  %>%
+  dplyr::filter(PValue <=0.05)%>%
+  dplyr::filter(abs(IncLevelDifference) >= 0.05) ->SRP070710_sig_rna
+
+read.table("/data/liull/immune-checkpoint-blockade/RNA_splicing/SRP094781/RI.MATS.JCEC.txt",header = T)  %>%
+  dplyr::filter(PValue <=0.05)%>%
+  dplyr::filter(abs(IncLevelDifference) >= 0.05) ->SRP094781_sig_rna
+
+read.table("/data/liull/immune-checkpoint-blockade/RNA_splicing/SRP150548/RI.MATS.JCEC.txt",header = T)  %>%
+  dplyr::filter(PValue <=0.05)%>%
+  dplyr::filter(abs(IncLevelDifference) >= 0.05) ->SRP150548_sig_rna
+#0
+
+#SE-----------------------------
+read.table("/data/liull/immune-checkpoint-blockade/RNA_splicing/SRP070710/SE.MATS.JCEC.txt",header = T)  %>%
+  dplyr::filter(PValue <=0.05)%>%
+  dplyr::filter(abs(IncLevelDifference) >= 0.05) ->SRP070710_sig_rna
+
+read.table("/data/liull/immune-checkpoint-blockade/RNA_splicing/SRP094781/SE.MATS.JCEC.txt",header = T)  %>%
+  dplyr::filter(PValue <=0.05)%>%
+  dplyr::filter(abs(IncLevelDifference) >= 0.05) ->SRP094781_sig_rna
+
+read.table("/data/liull/immune-checkpoint-blockade/RNA_splicing/SRP150548/SE.MATS.JCEC.txt",header = T)  %>%
+  dplyr::filter(PValue <=0.05)%>%
+  dplyr::filter(abs(IncLevelDifference) >= 0.05) ->SRP150548_sig_rna
+#95
+
+# venn.diagram(list(SRP070710=SRP070710_sig_rna$geneSymbol, SRP094781=SRP094781_sig_rna$geneSymbol,
+#                   SRP150548=SRP150548_sig_rna$geneSymbol),filename=NULL,fill = c("cornflowerblue", "green", "yellow"))->venn_melanoma_PD1
+# grid.draw(venn_melanoma_PD1)
 
 union(SRP070710_sig_rna$geneSymbol,SRP094781_sig_rna$geneSymbol)%>%
-  union(SRP150548_sig_rna$geneSymbol)%>%
-  union(ERP107734_sig_rna$geneSymbol)->all_genes
-
-read.table("/data/liull/reference/EntrezID_Symbl_EnsemblID_NCBI.txt",sep="\t",header = T,as.is = TRUE) ->relationship
-data.frame(all_genes)%>%
-  merge(relationship,by.x="all_genes",by.y="Symbol")->all_genes_frame
-
-enrichPathway(gene=all_genes_frame$GeneID,pvalueCutoff=1, readable=T,organism = "human")->eReactome
-dotplot(eReactome, showCategory=20)
+  union(SRP150548_sig_rna$geneSymbol)->all_genes
+intersect(SRP070710_sig_rna$geneSymbol,SRP094781_sig_rna$geneSymbol)%>%
+  intersect(SRP150548_sig_rna$geneSymbol)->interaction_splicing_gene
 
 
+#DEG genes
+read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/PD1_down_ENSG.txt",
+           header = T)->melanoma_PD1_down
+read.table("/data/liull/immune-checkpoint-blockade/New_batch_effect_pipeline/melanoma_PD1/PD1_up_ENSG.txt",
+           header = T)->melanoma_PD1_up
+
+union(melanoma_PD1_down$Symbol,melanoma_PD1_up$Symbol) -> all_DEG
+intersect(all_DEG,all_genes)
+
+
+#enrichment
+# read.table("/data/liull/reference/EntrezID_Symbl_EnsemblID_NCBI.txt",sep="\t",header = T,as.is = TRUE) ->relationship
+# data.frame(interaction_splicing_gene)%>%
+#   merge(relationship,by.x="interaction_splicing_gene",by.y="Symbol")->all_genes_frame
 
 
 
-#CTLA4
-read.table("/data/liull/immune-checkpoint-blockade/RNA_splicing/SRP011540/SE.MATS.JCEC.txt",header = T) -> SRP011540_A3SS_JCEC
-SRP011540_A3SS_JCEC %>%
-  dplyr::filter(PValue <=0.05)%>%
-  dplyr::filter(abs(IncLevelDifference) >= 0.05) ->SRP011540_sig_rna
